fb9594a3c963f3552d02b44fe0b4d18160bfd0b6
shoaib <shoaib.ahmed@gaditek.com>
2022-04-26 01:23:06 +0500

    url shortening library added and files published

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,29 @@ use App\Http\Controllers\WebScrapperController;
 */
 
 
+Route::get('/', function () {
+    $urls = \AshAllenDesign\ShortURL\Models\ShortURL::latest()->get();
+    return view('welcome', compact('urls'));
+});
 
+Route::post('/', function () {
+    $builder = new \AshAllenDesign\ShortURL\Classes\Builder();
+
+    $shortURLObject = $builder->destinationUrl(request()->url)->make();
+    $shortURL = $shortURLObject->default_short_url;
+
+    return back()->with('success','URL shortened successfully. ');
+
+})->name('url.shorten');
+
+Route::post('{id}', function ($id) {
+    $url = \AshAllenDesign\ShortURL\Models\ShortURL::find($id);
+    $url->url_key = request()->url;
+    $url->destination_url = request()->destination;
+    $url->save();
+
+    return back()->with('success','URL updated successfully. ');
+})->name('update');
 
 
 Route::get('home', [WebScrapperController::class, 'index']);
